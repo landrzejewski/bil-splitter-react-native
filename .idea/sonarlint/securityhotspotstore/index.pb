0502dbf375266ec8c47162387efb98e3aa411539
landrzejewski <landrzejewski.poczta@gmail.com>
2023-06-05 16:51:30 +0200

    final

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+8
+app.json,6\5\65596286f14dbaba6a5081a2edaae69700cc3c3b
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+6
+App.js,a\4\a4a49f7b4c549593514920b942b5a787f2773862
--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Platform, SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
+
+const FormField = (props) => (
+    <>
+        <Text style={props.labelStyle}>{props.labelText}</Text>
+        <TextInput style={props.inputStyle} value={props.value.toString()}
+                   onChangeText={props.onChange}
+                   placeholder={props.placeholderText}/>
+    </>
+);
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [amount, setAmount] = useState(0)
+    const [persons, setPersons] = useState(1)
+    const [tip, setTip] = useState(10)
+
+    const calculate = () => {
+        return (+amount + (amount * (tip / 100))) / persons;
+    };
+
+    return (
+        <View style={styles.container}>
+            <SafeAreaView style={styles.androidSafeArea}>
+                <FormField labelStyle={styles.label} inputStyle={styles.input}
+                           labelText="Kwota" placeholderText="Podaj wartość rachunku"
+                           value={amount} onChange={(value) => setAmount(value)}/>
+                <FormField labelStyle={styles.label} inputStyle={styles.input}
+                           labelText="Ilość osób" placeholderText="Podaj ilość osób"
+                           value={persons} onChange={setPersons}/>
+                <FormField labelStyle={styles.label} inputStyle={styles.input}
+                           labelText="Napiwek w procentach" placeholderText="Podaj wielkość napiwku w procentach"
+                           value={tip} onChange={setTip}/>
+                <Text>Do zapłaty na osobę: {calculate()} zł</Text>
+            </SafeAreaView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        padding: 20
+    },
+    label: {
+        fontSize: 14,
+    },
+    input: {
+        fontSize: 14,
+        backgroundColor: 'lightgray'
+    },
+    androidSafeArea: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? 50 : 0
+    }
 });
